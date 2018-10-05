cbad7404a5236575897a4d681d9a42cfa158acfa
Developer <Dev@jacobs-macbook-pro.home>
2018-10-05 09:28:04 +0200

    Hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,13 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi I'm Jake Kemsley</h1>
-    <p>a freelance digital <br />designer and developer <br />currently based in <br />Vaud, Switzerland</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className='hero'>
+      <div className='heroGroup'>
+        <h1>Hi, I'm Jake Kemsley</h1>
+        <p>a freelance digital <br />designer and developer <br />currently based in <br />Vaud, Switzerland</p>
+        <Link to="/page-2/">See my work</Link>
+      </div>
+    </div>
   </Layout>
 )
 
