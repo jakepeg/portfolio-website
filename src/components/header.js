f84f434038fdd1a7177abf606b6e4484fd04ac66
Developer <Dev@jacobs-macbook-pro.home>
2018-10-07 10:38:50 +0200

    broken - shows blank page

--- src/components/header.js
+++ src/components/header.js
@@ -2,30 +2,13 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="header">
+    <div className="headerGroup">
+      <link to="#Work">Selected Works</link>
+      <link to="#Work">My Toolbox</link>
+      <link to="#Work">Testimonials</link>
+      <link to="#Work">Contact</link>
+      <link to="#Work">Hire Me</link>
     </div>
   </div>
 )
