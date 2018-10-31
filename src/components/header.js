77b7386e2ab793c3915c57e675e8f415347562ae
Developer <Dev@jacobs-macbook-pro.home>
2018-10-31 14:07:33 +0100

    card component

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import './header.css'
 
 const Header = ({ siteTitle }) => (
-  <div className="header">
-    <div className="headerGroup">
-      <link to="#Work">Selected Works</link>
-      <link to="#Work">My Toolbox</link>
-      <link to="#Work">Testimonials</link>
-      <link to="#Work">Contact</link>
-      <link to="#Work">Hire Me</link>
-    </div>
+<div className="Header">
+  <div className="HeaderGroup">
+    <Link to="/"><img src={require('../images/gatsby-icon.png')} width="30" /></Link>
+    <Link to="/courses">Portfolio</Link>
+    <Link to="/downloads">Toolbox</Link>
+    <Link to="/workshops">Contact</Link>
+    <Link to="/buy"><button>Hire Me</button></Link>
   </div>
+</div>
 )
 
 export default Header
