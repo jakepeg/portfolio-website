e07f8e31861edcd70a75cea7872fe236a65dbc3e
Developer <Dev@jacobs-macbook-pro.home>
2019-10-14 14:11:34 +0200

    fixed broken images

--- src/components/cardwebsite.js
+++ src/components/cardwebsite.js
@@ -3,18 +3,33 @@ import { Link } from 'gatsby'
 import './card.css'
 
 const Cardwebsite = props => (
-
-    <div className="Card" >
-        <img src={'https:' + props.poster} />
-        <a href={props.link} target="_blank">
-            <div className="Card-info">
-                <p><span className="Card-label">Client</span><br />{props.client}</p>
-                <p><span className="Card-label">Project</span><br />{props.project}</p>
-                <p><span className="Card-label">Role</span><br />{props.role}</p>
-                <p><span className="Card-label">Tech</span><br />{props.technology}</p>
-            </div>
-        </a>
-    </div>
+  <div className="Card">
+    <img src={props.poster} />
+    <a href={props.link} target="_blank">
+      <div className="Card-info">
+        <p>
+          <span className="Card-label">Client</span>
+          <br />
+          {props.client}
+        </p>
+        <p>
+          <span className="Card-label">Project</span>
+          <br />
+          {props.project}
+        </p>
+        <p>
+          <span className="Card-label">Role</span>
+          <br />
+          {props.role}
+        </p>
+        <p>
+          <span className="Card-label">Tech</span>
+          <br />
+          {props.technology}
+        </p>
+      </div>
+    </a>
+  </div>
 )
 
 export default Cardwebsite
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-import {Card} from '../components/card'
+import { Card } from '../components/card'
 import staticdata from '../../staticdata.json'
 import Cell from '../components/cell'
 import Header from '../components/header'
@@ -36,9 +36,7 @@ const IndexPage = ({ children, data }) => (
           }
         }
 
-				allContentfulProject(
-          sort:{ fields: [createdAt], order: ASC }
-          ) {
+        allContentfulProject(sort: { fields: [createdAt], order: ASC }) {
           edges {
             node {
               client
@@ -56,120 +54,126 @@ const IndexPage = ({ children, data }) => (
                 }
               }
               createdAt
-              }
+            }
           }
         }
       }
     `}
     render={data => (
       <>
-
-
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: data.site.siteMetadata.description },
+            {
+              name: 'description',
+              content: data.site.siteMetadata.description,
+            },
             { name: 'keywords', content: data.site.siteMetadata.keywords },
           ]}
         >
           <html lang="en" />
+          <script src={'../delaunator.min.js'} />
         </Helmet>
         <Header />
         {children}
 
+        <Hero />
+        <div id="work">
+          {' '}
+          <br />{' '}
+        </div>
 
-  <Hero />
-  <div id="work"> <br /> </div>
+        <div className="Cards">
+          <h2>Selected Works</h2>
 
+          {/* <Filter /> */}
 
-    <div className="Cards">
-        <h2>Selected Works</h2>
+          <div className="CardGroup">
+            <CardWebsite
+              poster="../aussie-poster.jpg"
+              client="WorkingParty Melbourne"
+              project="Aussie Website Redesign"
+              role="Frontend Developer"
+              technology="HTML5, CSS3, BEM, LESS, JS, GIT, Gulp"
+              link="https://www.aussie.com.au/"
+            />
 
-        {/* <Filter /> */}
+            <CardWebsite
+              poster="../nab-cards-poster.jpg"
+              client="Clemenger BBDO Melbourne"
+              project="NAB Cards Template"
+              role="Frontend Developer"
+              technology="HTML5, Bootstrap, JADE, LESS, JS, GIT, Gulp"
+              link="https://www.nab.com.au/personal/banking/credit-cards"
+            />
 
-        <div className="CardGroup">
+            <CardWebsite
+              poster="../esssuper-poster.jpg"
+              client="Hardwire Melbourne"
+              project="ESS Super Interactive SVG"
+              role="Developer / Animator"
+              technology="SVG, JS, GSAP, ScrollMagic"
+              link="https://www.esssuper.com.au/investments/investment-approach/investment-approach-tool"
+            />
 
+            <CardWebsite
+              poster="../airactive-poster.jpg"
+              client="AirActive"
+              project="Pomotional Webiste"
+              role="Frontend Developer"
+              technology="Wordpress"
+              link="http://airactiveapp.com"
+            />
 
-      <CardWebsite 
-          poster="//www.digz.com.au/portfolio/WorkingParty/Aussie/poster.jpg" 
-          client="WorkingParty Melbourne"
-          project="Aussie Website Redesign"
-          role="Frontend Developer"
-          technology="HTML5, CSS3, BEM, LESS, JS, GIT, Gulp"
-          link="https://www.aussie.com.au/"
-        />
+            <CardWebsite
+              poster="../meningococcal-poster.jpg"
+              client="Hardwire Melbourne"
+              project="GSK Know Meningococcal"
+              role="Frontend Developer"
+              technology="HTML5, CSS3, LESS, JS, GIT, Gulp"
+              link="https://www.knowmeningococcal.com.au/"
+            />
 
-      <CardWebsite 
-          poster="//www.digz.com.au/portfolio/Clemenger/NAB/creditcards/poster.jpg" 
-          client="Clemenger BBDO Melbourne"
-          project="NAB Cards Template"
-          role="Frontend Developer"
-          technology="HTML5, Bootstrap, JADE, LESS, JS, GIT, Gulp"
-          link="https://www.nab.com.au/personal/banking/credit-cards"
-        />
+            <CardWebsite
+              poster="../nab-loans-poster.jpg"
+              client="Clemenger BBDO Melbourne"
+              project="NAB Loans Template"
+              role="Frontend Developer"
+              technology="HTML5, Bootstrap, JADE, LESS, JS, GIT, Gulp"
+              link="https://www.nab.com.au/personal/loans/personal-loans"
+            />
 
-      <CardWebsite 
-          poster="//www.digz.com.au/portfolio/hardwire/esssuper/poster.jpg" 
-          client="Hardwire Melbourne"
-          project="ESS Super Interactive SVG"
-          role="Developer / Animator"
-          technology="SVG, JS, GSAP, ScrollMagic"
-          link="https://www.esssuper.com.au/investments/investment-approach/investment-approach-tool"
-        />
-
-      <CardWebsite 
-          poster="//www.digz.com.au/portfolio/AirActive/website/poster.jpg" 
-          client="AirActive"
-          project="Pomotional Webiste"
-          role="Frontend Developer"
-          technology="Wordpress"
-          link="http://airactiveapp.com"
-        />
-
-      <CardWebsite 
-          poster="//www.digz.com.au/portfolio/hardwire/meningococcal/poster.jpg" 
-          client="Hardwire Melbourne"
-          project="GSK Know Meningococcal"
-          role="Frontend Developer"
-          technology="HTML5, CSS3, LESS, JS, GIT, Gulp"
-          link="https://www.knowmeningococcal.com.au/"
-        />
-
-      <CardWebsite 
-          poster="//www.digz.com.au/portfolio/Clemenger/NAB/loans/poster.jpg" 
-          client="Clemenger BBDO Melbourne"
-          project="NAB Loans Template"
-          role="Frontend Developer"
-          technology="HTML5, Bootstrap, JADE, LESS, JS, GIT, Gulp"
-          link="https://www.nab.com.au/personal/loans/personal-loans"
-        />
-      
-      {data.allContentfulProject.edges.map(edge => {
-      //const windowGlobal = typeof window !== 'undefined' && window
-      //const param = windowGlobal.location.search.slice(1);
-      return <Card client={edge.node.client}
-              project={edge.node.project}
-              role={edge.node.role}
-              technology={edge.node.technology}
-              poster={edge.node.poster.file.url}
-              id={edge.node.id}
-              link={edge.node.link}
-              createdAt={edge.node.createdAt}
-              projecttype={edge.node.type}
-              width={edge.node.width}
-              height={edge.node.height}
-              />
-      })}
-
-      </div>
-      </div>
-      <div id="skills"> <br /> </div>
-      <h2>Skills</h2>
-      <SectionCellGroup>
-        {staticdata.cells.map(cell => (
-          <Cell title={cell.title} image={cell.image} />
-        ))}
-      </SectionCellGroup>
+            {data.allContentfulProject.edges.map(edge => {
+              //const windowGlobal = typeof window !== 'undefined' && window
+              //const param = windowGlobal.location.search.slice(1);
+              return (
+                <Card
+                  client={edge.node.client}
+                  project={edge.node.project}
+                  role={edge.node.role}
+                  technology={edge.node.technology}
+                  poster={edge.node.poster.file.url}
+                  id={edge.node.id}
+                  link={edge.node.link}
+                  createdAt={edge.node.createdAt}
+                  projecttype={edge.node.type}
+                  width={edge.node.width}
+                  height={edge.node.height}
+                />
+              )
+            })}
+          </div>
+        </div>
+        <div id="skills">
+          {' '}
+          <br />{' '}
+        </div>
+        <h2>Skills</h2>
+        <SectionCellGroup>
+          {staticdata.cells.map(cell => (
+            <Cell title={cell.title} image={cell.image} />
+          ))}
+        </SectionCellGroup>
         <Footer />
       </>
     )}
