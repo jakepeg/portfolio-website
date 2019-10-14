4624fcdc69ba88e4fca80f45e63fe46384b04b76
Developer <Dev@jacobs-macbook-pro.home>
2019-10-14 14:21:33 +0200

    fixed images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,16 @@
 module.exports = {
   siteMetadata: {
     title: 'Jake Kemsley - Creative Digital Developer',
-    description: 'Customer focussed Digital All-Rounder, with a track record of successfully delivering awesome digital experiences.',
-    keywords: 'Web design, web designer, project manager, web developer, frontend developer, html5 banners, websites, apps, display, edms, games, microsites, landing pages'
+    description:
+      'Customer focussed Digital All-Rounder, with a track record of successfully delivering awesome digital experiences.',
+    keywords:
+      'Web design, web designer, project manager, web developer, frontend developer, html5 banners, websites, apps, display, edms, games, microsites, landing pages',
   },
   plugins: [
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-54646831-1",
+        trackingId: 'UA-54646831-1',
         head: true,
       },
     },
@@ -16,9 +18,10 @@ module.exports = {
     {
       resolve: 'gatsby-source-contentful',
       options: {
-        spaceId: 'hidden',
-        accessToken: 'hidden'
-      }
-    }
-  ]
+        spaceId: 'c66kaa3uliew',
+        accessToken:
+          '71fb3137adbf1df4e638d369fab160091410b78d4b08f8bf940a847c09a48cc6',
+      },
+    },
+  ],
 }
